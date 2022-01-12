e7aad11433a92f8cf8555cb783e4c84c466f3783
Germán <german.alzate970814@gmail.com>
2022-01-12 07:16:22 -0500

    Fixing package iphone development target number for compatibility with iOS 11

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('mediamonkpl', () => App);
-
-if (Platform.OS === 'web') {
-  const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication('mediamonkpl', { rootTag });
-}
+AppRegistry.registerComponent('main', () => App);
